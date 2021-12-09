a79cd6ea143882f9dde9cc72fae1f30afc7cd2f2
David Čeko <73242733+davidceko@users.noreply.github.com>
2021-12-10 00:54:53 +0100

    Update HomeComponent.js

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -5,19 +5,17 @@ function Naslovna() {
   return (
     <>
       <Jumbotron>
-        <div className="container">
-          <div className="row row-header">
-            <div className="col-12 col-sm-6">
-              <h1>Restoran dish5</h1>
-              <p>
-                {" "}
-                Neki super motivirajući marketinski tekst o dugogodisnjem
-                iskustvu i kulinarskom umijecu. Rezervirajte danas!
-              </p>
+            <div className="container">
+                <div className="row row-header">
+                    <div>
+                        <img src="assets/images/logo_white.png"></img>
+                    </div>
+                        <div className="col-12 col-sm-22">
+                            <p>Restoran dish5 je mjesto za beskrajne užitke. <br></br> Napravite rezervaciju ili nam se obratite za <br></br> organizaciju događaja.</p>
+                        </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </Jumbotron>
+        </Jumbotron>
     </>
   );
 }
