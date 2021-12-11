c584d0e6b486836c7d93dd3ec17b24445ee5604d
Netz <49691921+Netz00@users.noreply.github.com>
2021-12-11 19:50:27 +0100

    user seed hotfix, unique e-mail

--- server/seeds/01_users.js
+++ server/seeds/01_users.js
@@ -5,17 +5,17 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake1', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 2, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake2', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 3, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake3', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 4, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake4', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 5, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake5', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 6, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake6', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 7, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake7', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 8, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake8', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 9, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake9', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 10, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake10', firstname: 'Fake', lastname: 'User', type: 0},
-        {id: 11, email: 'test@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake11', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 1, email: 'test1@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake1', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 2, email: 'test2@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake2', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 3, email: 'test3@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake3', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 4, email: 'test4@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake4', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 5, email: 'test5@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake5', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 6, email: 'test6@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake6', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 7, email: 'test7@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake7', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 8, email: 'test8@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake8', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 9, email: 'test9@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake9', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 10, email: 'test10@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake10', firstname: 'Fake', lastname: 'User', type: 0},
+        {id: 11, email: 'test11@gmail.com', ip_addr: '192.168.0.1', password: 'test@gmail.com', username: 'fake11', firstname: 'Fake', lastname: 'User', type: 0},
       ]);
     });
 };
