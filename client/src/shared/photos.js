6ef89c61c2f9008b57236a7ae340572d3def1a5d
Veronika Dumic <veronika.dumic@gmail.com>
2021-12-14 18:01:30 +0100

    Change photos source

--- client/src/shared/photos.js
+++ client/src/shared/photos.js
@@ -2,11 +2,11 @@ export const PHOTOS = [
   {
     id: 0,
     name: "Fotografija 1",
-    image: "assets/images/Foto1.jpg",
+    image: "/assets/images/Foto1.jpg",
   },
   {
     id: 1,
     name: "Fotografija 2",
-    image: "assets/images/Foto2.jpg",
+    image: "/assets/images/Foto2.jpg",
   },
 ];
