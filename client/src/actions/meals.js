80fb1702c5f3eaa878ea9c90bc1e6e7993b9c3bf
Netz <49691921+Netz00@users.noreply.github.com>
2021-12-15 18:58:40 +0100

    meals action update

--- client/src/actions/meals.js
+++ client/src/actions/meals.js
@@ -2,11 +2,10 @@ import {
   DISHES_LOADING,
   END_LOADING,
   ADD_DISHES,
-} from "../constants/actionTypes";
-import * as api from "../api/index.js";
+} from '../constants/actionTypes';
+import * as api from '../api/index.js';
 
 export const getMeals = () => async (dispatch) => {
-  console.log("dsaasd");
   try {
     dispatch({
       type: DISHES_LOADING,
