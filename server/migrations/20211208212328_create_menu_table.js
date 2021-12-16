05c25285438e8e7d38f771861a464c86b54ca314
Netz <49691921+Netz00@users.noreply.github.com>
2021-12-16 19:47:46 +0100

    migrations update
    description removed from Meal and Menu, meal_require_groceries migration added

--- server/migrations/20211208212328_create_menu_table.js
+++ server/migrations/20211208212328_create_menu_table.js
@@ -2,7 +2,6 @@ exports.up = function (knex) {
   return knex.schema.createTable("menu", function (table) {
     table.increments("id").primary();
     table.string("title", [64]).unique().notNullable();
-    table.text("description");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
--- server/migrations/20211208212335_create_booking_table.js
+++ server/migrations/20211208212335_create_booking_table.js
@@ -2,8 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("booking", function (table) {
     table.increments("id").primary();
     table.integer("number_of_persons").unsigned().notNullable();
-    table.integer("table").notNullable();
-    table.string("fullname",[128]).notNullable();
+    table.string("fullname", [128]).notNullable();
     table.string("contact").notNullable();
     table.timestamp("time_of_arrival").defaultTo(knex.fn.now());
     table.timestamp("created_at").defaultTo(knex.fn.now());
