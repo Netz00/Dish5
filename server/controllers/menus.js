c136690d7766a034d2068e23763489bac9413352
Netz <49691921+Netz00@users.noreply.github.com>
2022-01-02 20:41:12 +0100

    added processing fetched menus with meals before response

--- server/controllers/menus.js
+++ server/controllers/menus.js
@@ -18,8 +18,23 @@ async function get(req, res) {
 
 async function getAll(req, res) {
   try {
-    const result = await MenuRepo.getAll();
-    return res.status(200).json(result);
+    //get array of menus with meals in subarray
+    //const result = await MenuRepo.getAll();
+
+    //get array of meals with menu_id for each meal
+    const result = await MenuRepo.getAllMeals();
+
+    const meals_with_menuId = [].concat.apply(
+      [],
+      result.map((menu) =>
+        menu.meals.map((meal) => ({
+          menu_id: menu.id,
+          ...meal,
+        }))
+      )
+    );
+
+    return res.status(200).json(meals_with_menuId);
   } catch (err) {
     return res.status(404).json({ message: error.message });
   }
--- server/services/db/operations/menu.js
+++ server/services/db/operations/menu.js
@@ -3,6 +3,7 @@ const Menu = require('../models/menu');
 module.exports = {
   get,
   getAll,
+  getAllMeals,
 };
 
 // get al menus
@@ -15,6 +16,17 @@ function get(id) {
   return Menu.query()
     .select('id', 'title')
     .findById(id)
+    .withGraphFetched('meals')
+    .modifyGraph('meals', (builder) => {
+      builder.select('id', 'title', 'price');
+    });
+}
+
+
+// get all meals for all menus
+function getAllMeals(id) {
+  return Menu.query()
+    .select('id', 'title')
     .withGraphFetched('meals')
     .modifyGraph('meals', (builder) => {
       builder.select('id', 'title', 'price');
