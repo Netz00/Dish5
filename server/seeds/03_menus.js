3a8ea555bbac38e42f4d0992615fffccdb1a2495
Netz <49691921+Netz00@users.noreply.github.com>
2022-01-03 20:49:03 +0100

    seed files updated and ID insert bug fixed
    after manually inserting id in meal/menu relations, id sequence started from 0 and caused unique exception

--- server/seeds/03_menus.js
+++ server/seeds/03_menus.js
@@ -1,13 +1,21 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('menu')
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('menu').insert([
-        { name: 'menu1' },
-        { name: 'menu2' },
-        { name: 'menu3' },
-      ]);
-    });
+  return (
+    knex('menu')
+      .del()
+      .then(function () {
+        // Inserts seed entries
+        return knex('menu').insert([
+          { id: 1, name: 'Juhe' },
+          { id: 2, name: 'Topla predjela' },
+          { id: 3, name: 'Glavna jela' },
+          { id: 4, name: 'Deserti' },
+          { id: 5, name: 'Hladna predjela' },
+        ]);
+      })
+      // reinitialise id_sey after manually inserting id-s
+      .then(function () {
+        return knex.raw("select setval('menu_id_seq', max(id)) from menu");
+      })
+  );
 };
