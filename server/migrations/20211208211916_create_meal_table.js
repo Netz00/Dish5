79a5d773f1d4652ee8c8be2be55f6624ca23e6da
Netz <49691921+Netz00@users.noreply.github.com>
2022-01-03 20:38:27 +0100

    description added to meal

--- server/migrations/20211208211916_create_meal_table.js
+++ server/migrations/20211208211916_create_meal_table.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('meal', function (table) {
     table.increments('id').primary();
     table.string('name', [64]).unique().notNullable();
+    table.text('description');
     table.float('price').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
